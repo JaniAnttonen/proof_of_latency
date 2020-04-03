b39601a5a3dbb2e9efff4be30ec14a79f654dabf
JaniAnttonen <jani.anttonen@protonmail.ch>
2020-04-03 21:53:13 +0300

    calculate somethingings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate vdf;
+use vdf::{VDFParams, WesolowskiVDFParams, VDF};
+
 fn main() {
-    println!("Hello, world!");
+    let num_bits: u16 = 2048;
+    let wesolowski_vdf = WesolowskiVDFParams(num_bits).new();
+
+    let solution = wesolowski_vdf.solve(b"\xaa", 10000).unwrap();
+    println!("{:?}", solution);
 }
