358ae15b0f12c18109323e21b7ad8cb542f80574
Jani Anttonen <jantto@utu.fi>
2020-09-29 13:02:19 +0300

    Use blake3 instead of sha

--- src/vdf/util.rs
+++ src/vdf/util.rs
@@ -1,14 +1,15 @@
+extern crate blake3;
+use blake3::Hash;
 use ramp::Int;
-use sha3::{Digest, Sha3_512};
 
+/// Deterministically hashes the input string s to be a member of the
+/// multiplicative group of modulo rsa_mod.
 pub fn hash(s: &str, rsa_mod: &Int) -> Int {
     let mut ans = Int::zero();
     for i in 0..(2 * rsa_mod.bit_length() / 512 + 1) {
-        let mut hasher = Sha3_512::new();
-        hasher.input(format!("{}{}", s, i).as_bytes());
-        let arr = hasher.result();
-        for x in arr.into_iter() {
-            ans = (ans << 8) + Int::from(x);
+        let hash: Hash = blake3::hash(format!("{}{}", s, i).as_bytes());
+        for x in hash.as_bytes().into_iter() {
+            ans = (ans << 8) + Int::from(*x);
         }
     }
     ans % rsa_mod
