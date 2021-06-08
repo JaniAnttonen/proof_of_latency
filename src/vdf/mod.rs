1081c6e1c239b07ae56ae709f210d7c7fdda0cea
Jani Anttonen <jantto@utu.fi>
2021-06-08 14:52:13 +0300

    Add benchmarks for comparing parallel and sequential Wesolowski VDF proof calculation

--- src/vdf/mod.rs
+++ src/vdf/mod.rs
@@ -1,3 +1,6 @@
+#![allow(soft_unstable)]
+extern crate test;
+
 use std::error::Error;
 use std::fmt;
 
@@ -28,6 +31,7 @@ mod tests {
     use ramp::Int;
     use ramp_primes::Generator;
     use std::{thread, time};
+    use test::Bencher;
 
     const RSA_2048: &str = "2519590847565789349402718324004839857142928212620403202777713783604366202070759555626401852588078440691829064124951508218929855914917618450280848912007284499268739280728777673597141834727026189637501497182469116507761337985909570009733045974880842840179742910064245869181719511874612151517265463228221686998754918242243363725908514186546204357679842338718477444792073993423658482382428119816381501067481045166037730605620161967625613384414360383390441495263443219011465754445417842402092461651572335077870774981712577246796292638635637328991215483143816789988504044536402352738195137863656439121201039712282120720357";
 
@@ -163,5 +167,54 @@ mod tests {
                 assert!(proof2.verify());
             }
         }
+    }
+
+    #[bench]
+    fn bench_sequential(b: &mut Bencher) {
+        let modulus = Int::from_str_radix(RSA_2048, 10).unwrap();
+        let hashablings2 = &"ghsalkghsakhgaligheliah<lifehf esipf";
+        let root_hashed = util::hash(&hashablings2.to_string(), &modulus);
+        let cap_str = Generator::new_safe_prime(64).to_str_radix(10, false);
+        b.iter(|| {
+            let cap = Int::from_str_radix(&cap_str, 10).unwrap();
+            let vdf = evaluation::VDF::new(
+                modulus.clone(),
+                root_hashed.clone(),
+                256,
+                proof::ProofType::Sequential,
+            )
+            .with_cap(cap);
+
+            let (_capper, receiver) = vdf.run_vdf_worker();
+
+            let res = receiver.recv();
+            if res.is_err() {
+                panic!("could not receive proof");
+            }
+        })
+    }
+    #[bench]
+    fn bench_parallel(b: &mut Bencher) {
+        let modulus = Int::from_str_radix(RSA_2048, 10).unwrap();
+        let hashablings2 = &"ghsalkghsakhgaligheliah<lifehf esipf";
+        let root_hashed = util::hash(&hashablings2.to_string(), &modulus);
+        let cap_str = Generator::new_safe_prime(64).to_str_radix(10, false);
+        b.iter(|| {
+            let cap = Int::from_str_radix(&cap_str, 10).unwrap();
+            let vdf = evaluation::VDF::new(
+                modulus.clone(),
+                root_hashed.clone(),
+                256,
+                proof::ProofType::Parallel,
+            )
+            .with_cap(cap);
+
+            let (_capper, receiver) = vdf.run_vdf_worker();
+
+            let res = receiver.recv();
+            if res.is_err() {
+                panic!("could not receive proof");
+            }
+        })
     }
 }
